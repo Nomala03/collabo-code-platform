b865733eef32914c066f33cdbeabacdc70e90b23
nomala03 <mbalimadonsela03@gmail.com>
2025-10-06 15:13:14 +0200

    renaming file

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,11 +1,5 @@
-import { Role } from '@prisma/client';
 import { TokenPayload } from '../../utils/jwt';
 
-export interface JwtPayload {
-  id: string;
-  email: string;
-  role: Role;
-}
 
 //Express Request includes `user`
 declare global {
